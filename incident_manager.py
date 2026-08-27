a4df9b5672a3df5eef223286437bf15298293aca
Mahmoud Ayman Noureddine <memonour2002@outlook.com>
2026-08-27 21:00:31 +0300

    Update incident_manager.py

--- incident_manager.py
+++ incident_manager.py
@@ -2,40 +2,88 @@ import json
 from datetime import datetime
 
 
-incident_type = input("Incident Type: ")
+def calculate_priority(severity):
 
-severity = input(
-    "Severity (Critical/High/Medium/Low): "
-)
+    severity = severity.lower()
 
-description = input("Description: ")
+    if severity == "critical":
+        return "P1"
 
+    elif severity == "high":
+        return "P2"
 
-incident = {
-    "id": "INC-1001",
-    "type": incident_type,
-    "severity": severity,
-    "status": "Open",
-    "description": description,
-    "created_at": str(datetime.now())
-}
+    elif severity == "medium":
+        return "P3"
+
+    else:
+        return "P4"
 
 
-with open("incidents.json", "r") as file:
+def get_next_incident_id():
 
-    incidents = json.load(file)
+    try:
+
+        with open("incidents.json", "r") as file:
+
+            incidents = json.load(file)
+
+            return f"INC-{1001 + len(incidents)}"
+
+    except:
+
+        return "INC-1001"
 
 
-incidents.append(incident)
+def create_incident():
+
+    incident_type = input("Incident Type: ")
+
+    severity = input("Severity (Critical/High/Medium/Low): ")
+
+    description = input("Description: ")
+
+    priority = calculate_priority(severity)
+
+    incident = {
+
+        "id": get_next_incident_id(),
+
+        "type": incident_type,
+
+        "severity": severity.title(),
+
+        "priority": priority,
+
+        "status": "Open",
+
+        "description": description,
+
+        "created_at": str(datetime.now())
+    }
+
+    try:
+
+        with open("incidents.json", "r") as file:
+
+            incidents = json.load(file)
+
+    except:
+
+        incidents = []
+
+    incidents.append(incident)
+
+    with open("incidents.json", "w") as file:
+
+        json.dump(incidents, file, indent=4)
+
+    print("\nINCIDENT SAVED SUCCESSFULLY")
+
+    print("=" * 50)
+
+    for key, value in incident.items():
+
+        print(f"{key}: {value}")
 
 
-with open("incidents.json", "w") as file:
-
-    json.dump(incidents, file, indent=4)
-
-
-print("\nINCIDENT SAVED SUCCESSFULLY")
-
-print("=" * 50)
-
-print(incident)
+create_incident()
