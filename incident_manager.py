bfb7447acc87ed3142e64f6f66cd3da6e24e3a0e
Mahmoud Ayman Noureddine <memonour2002@outlook.com>
2026-08-27 20:55:23 +0300

    Update incident_manager.py

--- incident_manager.py
+++ incident_manager.py
@@ -2,29 +2,40 @@ import json
 from datetime import datetime
 
 
-def create_incident():
+incident_type = input("Incident Type: ")
 
-    incident_type = input("Incident Type: ")
+severity = input(
+    "Severity (Critical/High/Medium/Low): "
+)
 
-    severity = input("Severity (Critical/High/Medium/Low): ")
-
-    description = input("Description: ")
-
-    incident = {
-        "id": "INC-1001",
-        "type": incident_type,
-        "severity": severity,
-        "status": "Open",
-        "description": description,
-        "created_at": str(datetime.now())
-    }
-
-    print("\nINCIDENT CREATED")
-
-    print("=" * 40)
-
-    for key, value in incident.items():
-        print(f"{key}: {value}")
+description = input("Description: ")
 
 
-create_incident()
+incident = {
+    "id": "INC-1001",
+    "type": incident_type,
+    "severity": severity,
+    "status": "Open",
+    "description": description,
+    "created_at": str(datetime.now())
+}
+
+
+with open("incidents.json", "r") as file:
+
+    incidents = json.load(file)
+
+
+incidents.append(incident)
+
+
+with open("incidents.json", "w") as file:
+
+    json.dump(incidents, file, indent=4)
+
+
+print("\nINCIDENT SAVED SUCCESSFULLY")
+
+print("=" * 50)
+
+print(incident)
